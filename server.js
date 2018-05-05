dbc5e4096ab1a4f9c4b48337cb103fd678f23ee6
easyAppsRepos <jralfarog@gmail.com>
2018-05-05 12:07:19 -0600

    alpha

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+// API file for interacting with MongoDB
+//const api = require('./server/routes/api');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+var cors = require('cors');
+
+// use it before all route definitions
+app.use(cors({origin: 'http://localhost:3003'}));
+
+
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'consolaAS/dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
