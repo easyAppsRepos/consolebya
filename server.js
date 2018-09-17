951a207c3e240d9471d2d901721dbe43d2fd9d7e
easyAppsRepos <jralfarog@gmail.com>
2018-09-17 03:19:18 -0600

    listol

--- server.js
+++ server.js
@@ -24,10 +24,26 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 // Send all other requests to the Angular app
+/*
 app.get('*', (req, res) => {
 	console.log(req.subdomains);
     res.sendFile(path.join(__dirname, './dist/index.html'));
 });
+*/
+
+app.get('*', (req, res) => {
+		console.log(req.subdomains);
+
+	if(req.subdomains[1]=='gestion'){
+		res.sendFile(path.join(__dirname, './gestion/dist/index.html'));
+	}
+	else{
+		res.sendFile(path.join(__dirname, './dist/index.html'));
+	}
+
+    
+});
+
 
 //Set Port
 const port = process.env.PORT || '80';
