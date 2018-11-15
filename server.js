8b51913d8edbc3d0817d5fc54544b84ea30c1aa3
easyAppsRepos <jralfarog@gmail.com>
2018-11-15 08:38:12 -0600

    dds

--- server.js
+++ server.js
@@ -89,7 +89,8 @@ const serverHttps = https.createServer(https_options,(req, res) => {
 	    res.writeHead(301,{Location: 'https://'+req.headers.host.replace(/^www\./, '')+req.url});
   		res.end();
 	}
-    else next();
+ 
+ 
 
 
 
