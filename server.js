ea302694afae279a8d1241d222ecc6a5c9b28467
easyAppsRepos <jralfarog@gmail.com>
2018-11-15 07:54:27 -0600

    dds

--- server.js
+++ server.js
@@ -80,14 +80,9 @@ var server = http.createServer((req, res) => {
   res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
   res.end();
 });
-//.replace(/^www\./, '')
 
-const serverHttps = https.createServer(https_options,(req, res) => {
-  res.writeHead(301,{Location: `https://`+req.headers.host.replace(/^www\./, '')+req.url});
-  res.end();
-});
 
-//const serverHttps = https.createServer(https_options,app);
+const serverHttps = https.createServer(https_options,app);
 
 serverHttps.listen(443, () => console.log(`Running on localhost:8443`));
 
